049f0beb6ecb62eb9ad1f6e7d875eac24fc18d02
fabio-c266 <fabiocaldas266@gmail.com>
2024-11-15 14:01:16 -0300

    feat: task repository

---
+++ src/main/java/com/fabiocaldas/tasks/api/repositories/TaskRepository.java
@@ -0,0 +1,33 @@
+package com.fabiocaldas.tasks.api.repositories;
+
+import com.fabiocaldas.tasks.api.entities.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TaskRepository extends JpaRepository<Task, Integer> {
+    boolean existsByName(String name);
+
+    @Query("""
+            SELECT t FROM Task t
+            order by t.position
+            """)
+    List<Task> findAllByPosition();
+
+    @Query("""
+            SELECT t.position FROM Task t
+            ORDER BY t.position DESC
+            LIMIT 1
+            """)
+    Optional<Integer> findLastPosition();
+
+
+    @Query("""
+            SELECT t FROM Task t 
+            WHERE t.position BETWEEN :start AND :end
+            """)
+    List<Task> findByPositionBetween(int start, int end);
+}
